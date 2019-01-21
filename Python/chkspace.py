7bfb02a3d593bbeafa89c527e3e5401cb36f4209
ZomboKid <38490271+ZomboKid@users.noreply.github.com>
2019-01-21 15:56:21 +0300

    Update chkspace.py

--- Python/chkspace.py
+++ Python/chkspace.py
@@ -1,38 +1,50 @@
 #! /usr/bin/python
 
-import sys, re, psutil, collections
+import sys
+import re
+import psutil
+import collections
 
-fs_patterns=['xfs','ext.*']
-fs_partitions_to_exclude=['boot','swap','tmp']
-trigger=int(sys.argv[1]) #set min GB free space in command line argument
-#---------------------------------------------------------------------
+fs_patterns = ['xfs', 'ext.*']
+fs_partitions_to_exclude = ['boot', 'swap', 'tmp']
+trigger = int(sys.argv[1])  # set min GB free space in command line argument
+# ----------------------------------------------------------------------------
+
+
 def f_mounted_fs_info_list():
-    diskinfo=collections.namedtuple('diskinfo','device mountpoint fstype free_gb')
-    fs_info_list=[]
-    for diskpart in psutil.disk_partitions(): #without arg 'All' psutil returns only phys dev
+    diskinfo = collections.namedtuple('diskinfo',
+                                      'device mountpoint fstype free_gb')
+    fs_info_list = []
+    # without arg 'All' psutil returns only phys dev
+    for diskpart in psutil.disk_partitions():
         for fs_pattern in fs_patterns:
             if re.compile(fs_pattern).match(diskpart.fstype):
                 for fs_partition in fs_partitions_to_exclude:
                     if fs_partition not in diskpart.mountpoint:
-                        contains=False
-                        continue 
+                        contains = False
+                        continue
                     else:
-                        contains=True
+                        contains = True
                         break
                 if not contains:
-                    fs_info_list.append(diskinfo(diskpart.device,diskpart.mountpoint,diskpart.fstype,int(round((psutil.disk_usage(diskpart.mountpoint).free)/1000000000))))
+                    fs_info_list.append(diskinfo(diskpart.device,
+                                        diskpart.mountpoint, diskpart.fstype,
+                                        int(round((psutil.disk_usage(
+                                         diskpart.mountpoint).free) /
+                                         1000000000))))
     return fs_info_list
-#---------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+
 def f_chkspace():
 
-    mounted_fs_info_list=f_mounted_fs_info_list()
-    
+    mounted_fs_info_list = f_mounted_fs_info_list()
+
     for i in mounted_fs_info_list:
         if i.free_gb < trigger:
-	    sys.exit("ERROR. On device "+i.device+" that is mounted to "+i.mountpoint+" is "+str(i.free_gb)+" GB free. It is less than "+str(trigger)+" GB.")
-#---------------------------------------------------------------------
-if __name__=="__main__":
+            sys.exit("ERROR. On device " + i.device + " that is mounted to " +
+                     i.mountpoint+" is " + str(i.free_gb) +
+                     " GB free. It is less than " + str(trigger) + " GB.")
+# ----------------------------------------------------------------------------
+if __name__ == "__main__":
     f_chkspace()
-
-
-
